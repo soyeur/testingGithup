7ffab6a0e99f1ccb96e2b5f84f5e9242ff71ad85
soyeur <soyeur_houssem@live.fr>
2012-12-27 14:21:05 +0100

    project java

---
+++ src/com/test/git/Car.java
@@ -0,0 +1,37 @@
+package com.test.git;
+
+public class Car {
+	
+	
+	private int mat;
+	
+	private String mark;
+	
+	private Boolean sold;
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public Boolean getSold() {
+		return sold;
+	}
+
+	public void setSold(Boolean sold) {
+		this.sold = sold;
+	}
+	
+
+}
